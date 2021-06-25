9a76c1839c75e2c3e233e445c573a72cbb370828
Lord Zarkares <73501736+LordZarkares@users.noreply.github.com>
2021-06-25 17:30:29 -0300

    :)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from math import asin, atan2
+from multiprocessing import Process
+from math            import asin, atan2, pi
+
 import pymem.process
 import keyboard
 import pymem
@@ -41,11 +43,16 @@ offset = {
     "dwLocalPlayer"                : (0xD892CC),
     "dwClientState"                : (0x588FEC),
     "dwClientState_ViewAngles"     : (0x4D90),
+    "dwForceAttack"                : (0x31D2690),
     "dwGlowObjectManager"          : (0x52EA5D0),
     "m_bDormant"                   : (0xED),
 
     "netvar"               : {
+        "m_fFlags"         : (0x104),
+        "m_iCrosshairId"   : (0xB3E8),
         "m_iTeamNum"       : (0xF4),
+        "m_iShotsFired"    : (0xA390),
+        "m_aimPunchAngle"  : (0x302C),
         "m_vecOrigin"      : (0x138),
         "m_dwBoneMatrix"   : (0x26A8),
         "m_vecViewOffset"  : (0x108),
@@ -59,7 +66,7 @@ def getPlayer(index: int) -> int:
     return pm.read_uint(client + offset["dwEntityList"] + (index * 0x10))
 
 def getLocalPlayer() -> int:
-    return pm.read_int(client + offset['dwLocalPlayer'])
+    return pm.read_uint(client + offset['dwLocalPlayer'])
 
 def getClientState() -> int:
     return pm.read_int(engine + offset['dwClientState'])
@@ -85,6 +92,9 @@ def sameTeam(playerA: int, playerB: int) -> bool:
 def isDead(player: int) -> bool:
     return getPlayerHealth(player) < 1 or getPlayerHealth(player) > 100
 
+def shotsFired(quantity: int):
+    return pm.read_uint(getLocalPlayer() + offset["netvar"]["m_iShotsFired"]) > quantity
+
 def isVisible(player: int) -> bool:
     clientState   = getClientState()
     localPlayerId = pm.read_int(clientState + offset['dwClientState_GetLocalPlayer'])
@@ -92,6 +102,14 @@ def isVisible(player: int) -> bool:
     spottedByMask = pm.read_int(player + offset['netvar']['m_bSpottedByMask'])
 
     return spottedByMask & (1 << localPlayerId)
+
+def normalizeAngles(x: float, y: float) -> tuple[float, float]:
+    if y >  180.0: y -= 360.0
+    if y < -180.0: y += 360.0
+    if x >   89.0: x -= 180.0
+    if x <  -89.0: x += 180.0
+
+    return (x, y)
 
 def getPlayerLocation(player: int) -> Vector3:
     return Vector3(
@@ -123,14 +141,28 @@ def getLocalPlayerViewAngles() -> Vector3:
     )
 
 def writeLocalPlayerViewAngles(x: float, y: float) -> None:
-    if y >  180.0: y -= 360.0
-    if y < -180.0: y += 360.0
-    if x >   89.0: x -= 180.0
-    if x <  -89.0: x += 180.0
+    x, y = normalizeAngles(x, y)
 
     clientState = getClientState()
     pm.write_float(clientState + offset['dwClientState_ViewAngles'] + 0x0, x)
     pm.write_float(clientState + offset['dwClientState_ViewAngles'] + 0x4, y)
+
+def normalizeRecoil(oldPunch: Vector3) -> tuple[Vector3, Vector3]:
+    localPlayer = getLocalPlayer()
+    
+    punchAngles = Vector3 (
+        x = pm.read_float(localPlayer + offset["netvar"]["m_aimPunchAngle"] + 0x0),
+        y = pm.read_float(localPlayer + offset["netvar"]["m_aimPunchAngle"] + 0x4),
+        z = pm.read_float(localPlayer + offset["netvar"]["m_aimPunchAngle"] + 0x8),
+    ) * 2
+
+    viewAnlges  = getLocalPlayerViewAngles()
+
+    newAngle = viewAnlges + oldPunch - punchAngles
+
+    writeLocalPlayerViewAngles(newAngle.x, newAngle.y)
+
+    return punchAngles
 
 def forceLocalPlayerAimTo(target: Vector3) -> None:
     localPlayerHead = getPlayerLocation(getLocalPlayer()) + getLocalPlayerViewOffset()
@@ -138,8 +170,8 @@ def forceLocalPlayerAimTo(target: Vector3) -> None:
     delta       = target - localPlayerHead
     deltaLength = localPlayerHead.distanceTo(target)
 
-    writeLocalPlayerViewAngles(-asin(delta.z / deltaLength) * (180.0 / 3.14159235368979), 
-                               atan2(delta.y , delta.x    ) * (180.0 / 3.14159235368979))
+    writeLocalPlayerViewAngles(-asin(delta.z / deltaLength) * (180.0 / pi), 
+                               atan2(delta.y , delta.x    ) * (180.0 / pi))
 
 def glowPlayer(player: int) -> None:
     entityGlow  = getPlayerGlowIndex(player)
@@ -181,18 +213,39 @@ def findClosestValidEnemy() -> bool or int:
 
     return False if closestDistanceIndex == -1 else closestDistanceIndex
 
+def aimbot():
+    entity = findClosestValidEnemy()
+
+    if entity:forceLocalPlayerAimTo(getPlayerBoneLocation(getPlayer(entity), bone = 8))
+
+def triggerbot():
+    entityId = pm.read_int(getLocalPlayer() + offset["netvar"]["m_iCrosshairId"])
+    entity   = getPlayer(entityId - 1)
+
+    if entityId > 0 and entityId <= 64 and not sameTeam(getLocalPlayer(), entity):
+        pm.write_int(client + offset["dwForceAttack"], 6)
+
+def wall():
+    for i in range(1, 32):
+        entity = getPlayer(i)
+
+        if entity:glowPlayer(entity)
+
 def main():
+
+    evilMode = False
+    oldPunch = Vector3(0, 0, 0)
+
     while True:
-        if keyboard.is_pressed('end'): exit(0)
+        if   keyboard.is_pressed('end')              : exit(0)
+        elif keyboard.is_pressed('f1')               : evilMode = not evilMode
+        elif keyboard.is_pressed('shift') or evilMode: aimbot()
 
-        if keyboard.is_pressed('shift'):
-            entity = findClosestValidEnemy()
+        triggerbot()
+            
+        if (shotsFired(1)): oldPunch = normalizeRecoil(oldPunch)
+        else              : oldPunch = Vector3(0, 0, 0)
 
-            if   entity:forceLocalPlayerAimTo(getPlayerBoneLocation(getPlayer(entity), bone = 8))
-
-        for i in range(1, 32):
-            entity = getPlayer(i)
-
-            if   entity:glowPlayer(entity)
+        wall()
             
 if __name__ == '__main__' : main()
